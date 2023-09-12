3057baadf1870b115995753e5c45bc05f21dd3a8
viacheslav.silchenko <viacheslav.silchenko@gmail.com>
2023-09-13 00:53:01 +0400

    add params definition file. update output. calculate checksum per params

--- api-checksum.py
+++ api-checksum.py
@@ -1,5 +1,8 @@
 import aiohttp
 import asyncio
+import time
+
+start_time = time.time()
 
 
 def checksum(element):
@@ -18,27 +21,46 @@ def calculate_checksum(data):
     return res
 
 
-async def get_single_api_checksum(session, url):
+async def get_api_checksum(session, url):
     async with session.get(url) as resp:
         api_resp = await resp.text()
-        return [url, calculate_checksum(api_resp)]
+        return calculate_checksum(api_resp)
+
+
+async def get_apis_checksum(session, urls, param_value):
+    tasks = []
+    for url in urls:
+        tasks.append(asyncio.ensure_future(get_api_checksum(session, url)))
+    checksums = await asyncio.gather(*tasks)
+    return [param_value, sum(checksums)]
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
+        with open('urls.txt', 'r') as urls_file:
+            urls_values = urls_file.read().splitlines()
+
+        with open('params.txt', 'r') as params_file:
+            params_values = params_file.read().splitlines()
         tasks = []
-        with open('urls.txt', 'r') as file:
-            urls = file.read().splitlines()
-            for url in urls:
-                tasks.append(asyncio.ensure_future(get_single_api_checksum(session, url)))
+        for param_value in params_values:
+            urls = []
+            for url_value in urls_values:
+                urls.append(url_value.format(param_value))
+
+            tasks.append(asyncio.ensure_future(get_apis_checksum(session, urls, param_value)))
 
         result = await asyncio.gather(*tasks)
         total_checksum = 0
+        print("checksum calculated for all apis for each param")
         for r in result:
-            print(r[0], r[1])
+            print(f"param: {r[0]}, checksum: {r[1]}")
             total_checksum += r[1]
 
-        print("Total checksum: ", total_checksum)
+        print(f"checksum for all apis for all params: {total_checksum}")
 
 
 asyncio.run(main())
+
+runtime = time.time() - start_time
+print(f"done in {runtime}s")
